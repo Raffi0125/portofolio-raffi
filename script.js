ab1eaf809de5417ee49de26c4ba11506d3f4a866
Raffi0125 <raffiathaillah0125@gmail.com>
2025-05-17 23:51:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // script.js
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const navbar = document.querySelector(".navbar");
+
+  menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    menuIcon.classList.toggle("bx-x");
+  });
+
+  document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
       e.preventDefault();
       document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
+        behavior: 'smooth'
       });
+      navbar.classList.remove("active");
+      menuIcon.classList.remove("bx-x");
+    });
   });
 });
